919dc62b74cc558d89d91603d0d29766128fb749
mtullo27 <36345087+mtullo27@users.noreply.github.com>
2018-10-12 22:13:40 -0400

    created sort_players

--- poker.c
+++ poker.c
@@ -1,5 +1,20 @@
 #include "objects.h"
 
+player[3] sort_players(player p1, player p2, player p3){
+  player ret[3] = {p1,p2,p3};
+  player temp;
+  int i,j;
+  for(i = 0; i<sizeof(ret)-1; i++){
+    for(j = 0; j<sizeof(ret)-i-1; j++){
+      if(ret[j]> ret[j+1]){
+        temp = ret[j];
+        ret[j] = ret[j+1];
+        ret[j+1] = temp;
+    }
+  }
+  return ret;
+}
+
 int main(){
   return 0;
 }
