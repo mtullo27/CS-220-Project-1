2ef18834984314f6c017f5a0bf70a639980d100a
mtullo27 <36345087+mtullo27@users.noreply.github.com>
2018-10-16 23:19:43 -0400

    Added method to get values of HAnds
    Optimized get high card

--- poker.c
+++ poker.c
@@ -1,4 +1,28 @@
- if(p.h[j].rank<p.h[j+1].rank){
+#include "objects.h"
+
+//this function sorts an array of players in descending order based on thier hand totals
+//This is done to make output easier. First slot in array is the winner. 
+void sort_players(player players[3]){
+  player temp;
+  int i,j;
+  for(i = 0; i<2; i++){
+    for(j = 0; j<3-i-1; j++){
+      if(players[j].total> players[j+1].total){
+        temp = players[j];
+        players[j] = players[j+1];
+        players[j+1] = temp;
+      }
+    }
+  }
+}
+
+//Sorts hand in ascending order. Makes finding pairs and straights easier
+void sort_hand(hand p){
+  card temp;
+  int i,j;
+  for(i = 0; i<4; i++){
+    for(j = 0; j<5-i-1; j++){
+      if(p.h[j].rank<p.h[j+1].rank){
         temp = p.h[j];
         p.h[j] = p.h[j+1];
 	p.h[j+1] = temp;
@@ -24,21 +48,30 @@ int straight(hand handy){
     return 0;
 }
 
-//Returns 1 if hand is a flush and 0 if otherwise
+//Returns 1 if a hand is a flush and 0 if otherwise.
 int flush(hand handy){
   int i;
   int sum = 1;
-  char suit1 = handy.h[0].suit;
-  if(straight(handy)){
-   for(i = 1; i<5; i++){
-     if(handy.h[i].suit == suit1)
-        sum++; 
-   }
+  char start = handy.h[0].suit;
+  for(i == 1; i<5; i++){
+     if(handy.h[i].suit == start)
+	sum++;
   }
   if(sum == 5)
     return 1;
   else 
     return 0;
+}
+//Returns 1 if hand is a flush and 0 if otherwise
+int straightFlush(hand handy){
+  int i;
+  int sum = 1;
+  char suit1 = handy.h[0].suit;
+  if(straight(handy)){
+    if(flush(handy))
+       return 1;
+  }
+  return 0;
 }
 
 //Returns 1 if hand is a royal flush and 0 if otherwise
@@ -126,28 +159,57 @@ int fullHouse(hand handy){
 }
 
 //finds the high card for a player given a player and the pile
-//puts all available cards into array and sorts it in ascending order
-//first card in array is the high card
+//puts all cards player can have into an array and finds the highest
+//rank of all the cards
 card highCard(pile piley, player playery){
   card cardy[7];
-  card temp;
-  int i,j,k;
+  card max;
+  int i,j;
   for(i = 0; i<sizeof(cardy); i++){
     if(i<5)
       cardy[i] = piley.p[i];
     else
       cardy[i] = playery.c[i-5];
   }
-  for(j = 0; j<6; j++){
-    for(k = 0; k<7-j-1; k++){
-      if(cardy[k].rank>cardy[k+1].rank){
-        temp = cardy[k];
-        cardy[k] = cardy[k+1];
-	cardy[k+1] = temp;
-      } 
-    }
+  max = cardy[0];
+  for(j = 1; j<7; j++){
+    if(cardy[i].rank>max.rank)
+      max = cardy[i];
   }
-  return cardy[0]; 
+  return max; 
+}
+
+//given a hand, returns the designated value for the given hand.
+// RF = 135 SF = 120 Four = 105 FH = 90 Flush = 75 S = 60 Three = 45 DP = 30 SP = 15
+//returns 0 if only high card
+int getValue(player playery){
+  hand handy = playery.h;
+  int highy = playery.high.rank;
+  if(straight(handy)){
+    if(straightFlush(handy)){
+      if(royalFlush(handy))
+        return 135+highy;
+      return 120+highy;
+     }
+  return 60+highy;
+  }
+  if(soloPair(handy)){
+    if(doublePair(handy))
+      return 30+highy;
+    return 15+highy;
+  }
+  if(three(handy)){
+    if(fullHouse(handy))
+      return 90+highy;
+    return 45+highy;
+  }
+  if(four(handy)){
+    return 105+highy;
+  }
+  if(flush(handy)){
+    return 75+highy;
+  }
+  return highy;
 }
 
 int main(){
