193b6f14c777c7dc33fa21c576aa408e91a2dce5
mtullo27 <36345087+mtullo27@users.noreply.github.com>
2018-10-20 17:01:37 -0400

    Finished poker.c

--- poker.c
+++ poker.c
@@ -3,83 +3,71 @@
 #include <stdio.h>
 #include <string.h>
 
-//helper method for sorting cards
-void swapC(card *c1, card *c2)
-{
-	card temp;
-	memcpy(&temp, c1, sizeof(card));
-	memcpy(c1, c2, sizeof(card));
-	memcpy(c2, &temp, sizeof(card));
-}
 
-//helper method for sorting players
-void swapP(player *p1, player *p2)
-{
-	player temp = *p1;
-	*p1 = *p2;
-	*p2 = temp;
-}
-
-//helper method for sorting hands
-void swapH(hand *h1, hand *h2)
-{
-	hand temp = *h1;
-	*h1 = *h2;
-	*h2 = temp;
-}
-//this function sorts an array of players in descending order based on thier hand totals
-//This is done to make output easier. First slot in array is the winner. 
-void sort_players(player players[3]){
-  int i,j;
-  for(i = 0; i<2; i++){
-    for(j=0; j<2-i; j++){
-			if(players[j].total == players[j+1].total){
-				if(players[j].high.rank < players[j+1].high.rank){
-					swapP(&players[j], &players[j+1]);
-				}
-			}
-			else{
-				if(players[j].total < players[j+1].total){
-					swapP(&players[j], &players[j+1]);
-				}
-			}
-		}
-  }
-}
-//picks the best hand for a player out of 21 possible hand
-hand* pick_hand(hand handy[21]){
-	int i,j;
-	for(i = 0; i<20; i++){
-		for(j = 0; j<20-i; j++){
-			if(handy[j].total == handy[j+1].total){
-				if(handy[j].h[4].rank < handy[j+1].h[4].rank){
-					swapH(&handy[j], &handy[j+1]);
-				}
-			}
-			else{
-				if(handy[j].total < handy[j+1].total){
-					swapH(&handy[j], &handy[j+1]);
-				}
-			}
-		}
-	}
-	hand *p;
-	p = &handy[0];
-	return p;
-}
-//Sorts hand in ascending order. Makes finding pairs and straights easier
-void sort_hand(hand p[5]){
+//Sorts cards in ascending order. Makes finding pairs and straights easier
+void sort_cards(card *p, int size){
   int i,j;
 	card temp;
-  for(i = 0; i<4; i++){
-    for(j = 0; j<4-i; j++){
+  for(i = 0; i<size-1; i++){
+    for(j = 0; j<size-i-1; j++){
       if(p[j].rank>p[j+1].rank){
-				swapC(&(p[j]), &(p[j+1]));
+				temp = p[j+1];
+        p[j+1] = p[j];
+        p[j] = temp;
       } 
     }
   }
 }
+//Returns value of pair if hand is only one pair and 0 otherwise
+int soloPair(hand handy){
+  int i;
+  for(i = 0; i<4; i++){
+    if(handy.h[i].rank == handy.h[i+1].rank)
+      return handy.h[i].rank;
+  }
+  return 0;
+}
+//returns last occurance of the rank in a pair to make finding double pairs easier
+//also makes finding highest pair easier
+int indexPair(hand handy){
+  int r,i;
+  for(i = 0; i<4; i++){
+    if(handy.h[i].rank == handy.h[i+1].rank)
+      r = i+1;
+    }
+  return r;
+}
 
+//Returns value of the pairs if two pairs and 0 otherwise
+int doublePair(hand handy){
+  int i,high, low;
+	int sum = 0;
+    for(i = 0;i<4; i++){
+       if(handy.h[i].rank == handy.h[i+1].rank){
+				if(!low){        
+					low = handy.h[i+1].rank;	
+					sum++;
+				}
+				if(low){
+					high = handy.h[i+1].rank;
+					sum++;
+				}
+       }
+    }	
+		if(sum == 2){			
+			return high;
+		}
+  return 0;
+}
+int doublePairHelper(hand handy){
+	if(doublePair(handy)){
+		if(soloPair(handy)>doublePair(handy))
+			return doublePair(handy);
+		else
+			return soloPair(handy);
+	}
+	return 0;
+}
 //Returns 1 if a hand is a straight and 0 if otherwise.
 int straight(hand handy){
   int i;
@@ -89,13 +77,13 @@ int straight(hand handy){
      if(handy.h[i].rank == start+i)
 	sum++;
   }
-  if(start = 2 && handy.h[4].rank == 14){
+  if(start == 2 && handy.h[4].rank == 14){
     sum++;
 		handy.h[4].rank = 1;
-		sort_hand(handy.h);
+		sort_cards((handy.h), 5);
 	}
   if(sum == 5)
-    return 1;
+    return handy.h[0].rank;
   else 
     return 0;
 }
@@ -110,7 +98,7 @@ int flush(hand handy){
 	sum++;
   }
   if(sum == 5)
-    return 1;
+    return handy.h[4].rank;
   else 
     return 0;
 }
@@ -120,7 +108,7 @@ int straightFlush(hand handy){
   char suit1 = handy.h[0].suit;
   if(straight(handy)){
     if(flush(handy))
-       return 1;
+       return handy.h[0].rank;
   }
   return 0;
 }
@@ -130,7 +118,7 @@ int royalFlush(hand handy){
   if(straight(handy)){
     if(flush(handy)){
       if(handy.h[0].rank==10)
-        return 1;
+        return 10;
     }
   }
   return 0;
@@ -139,10 +127,10 @@ int royalFlush(hand handy){
 //Returns 1 if hand has four of a kind and 0 otherwise
 int four(hand handy){
   if(handy.h[0].rank == handy.h[3].rank){
-    return 1;
+    return handy.h[3].rank;
   }
   if(handy.h[1].rank == handy.h[4].rank){
-    return 1;
+    return handy.h[3].rank;
   }
   else 
     return 0;
@@ -151,61 +139,32 @@ int four(hand handy){
 //Returns 1 if hand is three of a kind and 0 otherwise
 int three(hand handy){
   if(handy.h[0].rank == handy.h[2].rank){
-    return 1;
+    return handy.h[1].rank;
   }
   if(handy.h[1].rank == handy.h[3].rank){
-    return 1;
+    return handy.h[2].rank;
   }
   if(handy.h[2].rank == handy.h[4].rank){
-    return 1;
+    return handy.h[3].rank;
   }
   else 
     return 0;
 } 
 
-//Returns value of pair if hand is only one pair and 0 otherwise
-int soloPair(hand handy){
-  int i;
-  for(i = 0; i<4; i++){
-    if(handy.h[i].rank == handy.h[i+1].rank)
-      return handy.h[i].rank;
-  }
-  return 0;
-}
-//returns last occurance of the rank in a pair to make finding double pairs easier
-//also makes finding highest pair easier
-int indexPair(hand handy){
-  int r,i;
-  if(soloPair(handy)){
-  for(i = 0; i<4; i++){
-    if(handy.h[i].rank == handy.h[i+1].rank)
-      r = i+1;
-    }
-  }
-  return r;
-}
-
-//Returns value of the pairs if two pairs and 0 otherwise
-int doublePair(hand handy){
-  int i,high;
-  if(soloPair(handy)){
-    if(indexPair(handy) == 4 || indexPair(handy) == 5)
-      return 0;
-    for(i = indexPair(handy);i<4; i++){
-       if(handy.h[i].rank == handy.h[i+1].rank){
-         high = handy.h[i].rank;
-       }
-    }
-		return soloPair(handy)+high;
-  }
-  return 0;
-}
 
 //returns 1 if hand is full house and 0 otherwise
 int fullHouse(hand handy){
   if(soloPair(handy)){
     if(three(handy)){
-      return 1;
+      return three(handy);
+    }
+  }
+  return 0;
+}
+int fullHouseHelper(hand handy){
+  if(soloPair(handy)){
+    if(three(handy)){
+      return soloPair(handy);
     }
   }
   return 0;
@@ -217,12 +176,35 @@ int fullHouse(hand handy){
 card highCard(pile piley, player playery){
   card cardy[7];
   card max;
+	hand handy = playery.h;
   int i,j;
   for(i = 0; i<5; i++){
       cardy[i] = piley.p[i];
   }
 	cardy[5] = playery.c1;
 	cardy[6] = playery.c2;
+	for(i = 0; i<7; i++){
+		if(soloPair(handy)){
+			if(cardy[i].rank = soloPair(handy))
+				cardy[i].rank = 0;
+		}
+		if(doublePair(handy)){ 
+			if(cardy[i].rank = doublePair(handy))
+				cardy[i].rank = 0;
+		}
+		if(three(handy)){ 
+			if(cardy[i].rank = three(handy))
+				cardy[i].rank = 0;
+		}
+		if(four(handy)){ 
+			if(cardy[i].rank = four(handy))
+				cardy[i].rank = 0;
+		}
+		if(fullHouse(handy)){ 
+			if(cardy[i].rank = fullHouse(handy))
+				cardy[i].rank = 0;
+		}
+	}
   max = cardy[0];
   for(j = 1; j<7; j++){
     if(cardy[i].rank>max.rank)
@@ -231,35 +213,158 @@ card highCard(pile piley, player playery){
   return max; 
 }
 
+int tieBreak(player p1, player p2){
+	card high1 = p1.h.h[4];
+	card high2 = p2.h.h[4];
+	if(doublePair(p1.h) == doublePair(p2.h)){
+		if(doublePairHelper(p1.h)>doublePairHelper(p2.h))
+			return 1;
+	}	
+	if(fullHouse(p1.h) == fullHouse(p2.h)){
+		if(fullHouseHelper(p1.h)>fullHouseHelper(p2.h)){
+			return 1;
+		}
+	}
+	if(high1.rank==high2.rank){
+	high1 = p1.h.h[3];
+	high2 = p2.h.h[3];
+	if(high1.rank==high2.rank){
+		high1 = p1.h.h[2];
+		high2 = p2.h.h[2];
+		if(high1.rank==high2.rank){
+			high1 = p1.h.h[1];
+			high2 = p2.h.h[1];
+				if(high1.rank==high2.rank){
+					high1 = p1.h.h[0];
+					high2 = p2.h.h[0];
+				}
+			}
+		}
+	}
+	if(high1.rank>high2.rank)
+		return 1;	
+	return 0;
+}
+int handBreak(hand p1, hand p2){	
+	int i;
+	if(doublePair(p1) == doublePair(p2)){
+		if(doublePairHelper(p1)>doublePairHelper(p2))
+			return 1;
+	}	
+	if(fullHouse(p1) == fullHouse(p2)){
+		if(fullHouseHelper(p1)>fullHouseHelper(p2)){
+			return 1;
+		}
+	}
+	card high1 = p1.h[4];
+	card high2 = p2.h[4];
+	if(high1.rank==high2.rank){
+		high1 = p1.h[3];
+		high2 = p2.h[3];
+		if(high1.rank==high2.rank){
+			high1 = p1.h[2];
+			high2 = p2.h[2];
+			if(high1.rank==high2.rank){
+				high1 = p1.h[1];
+				high2 = p2.h[1];
+					if(high1.rank==high2.rank){
+						high1 = p1.h[0];
+						high2 = p2.h[0];
+					}
+			}
+		}
+	}
+	if(high1.rank<high2.rank){
+		return 1;
+	}
+	for(i = 0; i<4; i++){
+		if(p1.h[i].rank<p2.h[i].rank)
+			return 1;
+	}
+	return 0;
+}
+//picks the best hand for a player out of 21 possible hand
+void pick_hand(hand *handy, int size){
+	int i,j;
+	hand temp;
+	for(i = 0; i<size-1; i++){
+		for(j = 0; j<size-i-1; j++){
+			if(handy[j].total == handy[j+1].total){
+				if(handBreak(handy[j], handy[j+1])){
+					temp = handy[j+1];
+        	handy[j+1] = handy[j];
+        	handy[j] = temp;
+				}
+			}
+			else{
+				if(handy[j].total < handy[j+1].total){
+					temp = handy[j+1];
+        	handy[j+1] = handy[j];
+        	handy[j] = temp;
+				}
+			}
+		}
+	}
+}
+
+//this function sorts an array of players in descending order based on thier hand totals
+//This is done to make output easier. First slot in array is the winner. 
+void sort_players(player *players, int size){
+  int i,j;
+	player temp;
+  for(i = 0; i<size-1; i++){
+    for(j=0; j<size-i-1; j++){			
+		if(players[j].total < players[j+1].total){
+					temp = players[j];
+					players[j] = players[j+1];
+					players[j+1] = temp;
+			}
+			if(players[j].total == players[j+1].total){
+				if(tieBreak(players[j], players[j+1])==0){
+					temp = players[j];
+					players[j] = players[j+1];
+					players[j+1] = temp;
+				}
+			}
+		}
+  }
+}
+
+
+
+
 //given a hand, returns the designated value for the given hand.
 // RF = 900 SF = 800 Four = 700 FH = 600 Flush = 500 S = 400 Three = 300 DP = 200 SP = 100
 //returns 0 if only high card
 int getValue(hand handy){
   if(straight(handy)){
     if(straightFlush(handy)){
-      if(royalFlush(handy))
+      if(royalFlush(handy)){
         return 900;
-      return 800+handy.h[4].rank;
+			}
+      return 800+straight(handy);
      }
-  return 400+handy.h[4].rank;
+  return 400+straight(handy);
   }
   if(soloPair(handy)){
-    if(doublePair(handy))
+    if(doublePair(handy)){
       return 200+doublePair(handy);
+		}
     return 100+soloPair(handy);
   }
   if(three(handy)){
-    if(fullHouse(handy))
-      return 600;
-    return 300+handy.h[2].rank;
+    if(fullHouse(handy)){
+      return (600+fullHouse(handy));
+		}
+    return (300+three(handy));
   }
   if(four(handy)){
-    return 700+handy.h[2].rank;
+    return (700+four(handy));
   }
   if(flush(handy)){
-    return 500;
+    return (500+flush(handy));
   }
-  return 0;
+  return handy.h[4].rank;
 }
 int count;
 
@@ -286,64 +391,88 @@ void makehands(card ca[], int n, int k, hand hands[]) {
 
 int main(){
 	
-	char game1[33];
-	FILE *fp = fopen("samplehands.txt", "r");
-	//while(fgets(game1, 32, fp)!=NULL) {
- 
-		fgets(game1, 33, fp);
+	char game1[256];
+	FILE *fp = fopen("sample_hands.txt", "r");
+	FILE *fp2 = fopen("output.txt", "w+");
+	int counter = 0;
+	while(fgets(game1, 300, fp)) {
 		player p1, p2, p3;
 		pile pi;
 		//changes the face cards into number values for easier comparing later
-		int i;
-		for(i = 0; i<32; i++) {
-			if(game1[i]==('T')){
-				game1[i] = 10;
-			}
-			if(game1[i]==('J')){
-				game1[i] = 11;
-			}
-			if(game1[i]==('Q')){
-				game1[i] = 12;
-			}
-			if(game1[i]==('K')){
-				game1[i] = 13;
-			}
-			if(game1[i]==('A')){
-				game1[i] = 14;
+		int zed;
+		int ranks[300];
+		for(zed = 0; zed < 300; zed++){
+			switch(game1[zed]){
+				case '2':
+					ranks[zed] = 2;
+					break;
+				case '3':
+					ranks[zed] = 3;
+					break;
+				case '4':
+					ranks[zed] = 4;
+					break;
+				case '5':
+					ranks[zed] = 5;
+					break;
+				case '6':
+					ranks[zed] = 6;
+					break;
+				case '7':
+					ranks[zed] = 7;
+					break;
+				case '8':
+					ranks[zed] = 8;
+					break;
+				case '9':
+					ranks[zed] = 9;
+					break;
+				case 'T':
+					ranks[zed] = 10;
+					break;
+				case 'J':
+					ranks[zed] = 11;
+					break;
+				case 'Q':
+					ranks[zed] = 12;
+					break;
+				case 'K':
+					ranks[zed] = 13;
+					break;
+				case 'A':
+					ranks[zed] = 14;
+					break;
+				default:
+					ranks[zed] = 0;
+					break;
 			}
 		}
-		int y;
-		for(y = 0; y<32; y++) {
-				printf("%c", game1[y]);
-		}
-		printf("\n");
 		//assigns the players cards and the piles cards
 		p1.c1.suit = game1[1];
-		p1.c1.rank = game1[0]-'0';
-
+		p1.c1.rank = ranks[0];
 		p1.c2.suit = game1[4];
-		p1.c2.rank = game1[3]-'0';
+		p1.c2.rank = ranks[3];
 
 		p2.c1.suit = game1[7];
-		p2.c1.rank = game1[6]-'0';
+		p2.c1.rank = ranks[6];
 		p2.c2.suit = game1[10];
-		p2.c2.rank = game1[9]-'0';
+		p2.c2.rank = ranks[9];
 		
 		p3.c1.suit = game1[13];
-		p3.c1.rank = game1[12]-'0';
+		p3.c1.rank = ranks[12];
 		p3.c2.suit = game1[16];
-		p3.c2.rank = game1[15]-'0';
+		p3.c2.rank = ranks[15];
 
 		pi.p[0].suit = game1[19];
-		pi.p[0].rank = game1[18]-'0';
+		pi.p[0].rank = ranks[18];
 		pi.p[1].suit = game1[22];
-		pi.p[1].rank = game1[21]-'0';
+		pi.p[1].rank = ranks[21];
 		pi.p[2].suit = game1[25];
-		pi.p[2].rank = game1[24]-'0';
+		pi.p[2].rank = ranks[24];
 		pi.p[3].suit = game1[28];
-		pi.p[3].rank = game1[27]-'0';
+		pi.p[3].rank = ranks[27];
 		pi.p[4].suit = game1[31];
-		pi.p[4].rank = game1[30]-'0';
+		pi.p[4].rank = ranks[30];
 		
 		card ca1[7] = {p1.c1, p1.c2, pi.p[0], pi.p[1], pi.p[2], pi.p[3], pi.p[4]};
 		card ca2[7] = {p2.c1, p2.c2, pi.p[0], pi.p[1], pi.p[2], pi.p[3], pi.p[4]};
@@ -357,28 +486,20 @@ int main(){
 		
 		int z;
 		for(z = 0; z<21; z++){
-			sort_hand(hands1->h[z]);	
-			//hands1[z].total = getValue(hands1[z]);
-			sort_hand(hands2->h[z]);
-			//hands2[z].total = getValue(hands2[z]);
-			sort_hand(hands3->h[z]);
-			//hands3[z].total = getValue(hands3[z]);
-		}	
-		int test, testy;
-		for(test=0; test<21; test++)
-		{
-			
-			for(testy=0; testy<5; testy++){
-				printf("%u ", hands1[test].h[testy].rank);
-			}
-			printf("\n");
-		}
+			sort_cards(hands1[z].h, 5);	
+			hands1[z].total = getValue(hands1[z]);
+			sort_cards(hands2[z].h, 5);	
+			hands2[z].total = getValue(hands2[z]);
+			sort_cards(hands3[z].h, 5);	
+			hands3[z].total = getValue(hands3[z]);
+		}			
+		pick_hand(hands1, 21);
+		pick_hand(hands2, 21);
+		pick_hand(hands3, 21);
+		p1.h = hands1[0];
+		p2.h = hands2[0];
+		p3.h = hands3[0];		
 
-
-		p1.h = *pick_hand(hands1);
-		p2.h = *pick_hand(hands2);
-		p3.h = *pick_hand(hands3);
-		
 		p1.high = highCard(pi, p1);
 		p2.high = highCard(pi, p2);
 		p3.high = highCard(pi, p3);
@@ -390,25 +511,21 @@ int main(){
 		p1.id = 1;
 		p2.id = 2;
 		p3.id = 3;
-		fclose(fp);
- 
-
-
-
-		printf("\n");
+    
+		p1.total = p1.h.total;    
+		p2.total = p2.h.total;
+		p3.total = p3.h.total;    
 
 		player gamey1[3] = {p1,p2,p3};
-		sort_players(gamey1);		
-		for(i = 0; i<5; i++) {
-			printf("%u ", p1.h.h[i].rank);
-		}
-		printf("\n");
+		sort_players(gamey1, 3);		
 
-		FILE *fp2 = fopen("output.txt", "w+");
-		fprintf(fp2, "%s %d %s", "Player ",  gamey1[0].id, " wins.");
-		fclose(fp2);
-		return 0;
+		fprintf(fp2, "%s %d %s", "Player ",  gamey1[0].id, " wins. ");
+		fprintf(fp2, "\n");
 	
+	}		
+	fclose(fp2);	
+	fclose(fp);
+	return 0;
 }
 	
 
